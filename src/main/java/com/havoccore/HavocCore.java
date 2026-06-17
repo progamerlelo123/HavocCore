8dd374c8fe974a73e4fb9865dab1efb734163f72
progamerlelo123 <tigerforth3@gmail.com>
2026-06-17 13:16:36 +0300

    Update HavocCore.java

--- src/main/java/com/havoccore/HavocCore.java
+++ src/main/java/com/havoccore/HavocCore.java
@@ -2,6 +2,7 @@ package com.havoccore;
 
 import com.havoccore.economy.AuctionManager;
 import com.havoccore.economy.EconomyManager;
+import com.havoccore.trade.TradeManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,12 +10,14 @@ public final class HavocCore extends JavaPlugin {
 
     private EconomyManager economyManager;
     private AuctionManager auctionManager;
+    private TradeManager tradeManager;
 
     @Override
     public void onEnable() {
         // Initialize Core Systems
         this.economyManager = new EconomyManager(this);
         this.auctionManager = new AuctionManager(this);
+        this.tradeManager = new TradeManager(this);
 
         // Start background tasks (Hourly payout loop)
         Bukkit.getScheduler().runTaskTimer(this, () -> {
@@ -24,7 +27,7 @@ public final class HavocCore extends JavaPlugin {
             });
         }, 0L, 72000L); // 72000 ticks = 1 hour
 
-        getLogger().info("HavocCore Anarchy Economy successfully enabled!");
+        getLogger().info("HavocCore Anarchy Economy systems successfully enabled!");
     }
 
     @Override
@@ -38,5 +41,9 @@ public final class HavocCore extends JavaPlugin {
 
     public AuctionManager getAuctionManager() {
         return auctionManager;
+    }
+
+    public TradeManager getTradeManager() {
+        return tradeManager;
     }
 }
